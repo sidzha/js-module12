a95d301334a1188979001d543a77cbd1e524b3e6
Shakhriyar <56295240+sidzha@users.noreply.github.com>
2019-11-26 11:54:58 +0300

    v 1.5
    final edits

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 'use sctrict';
 
 const ballElem = $('.js-ball'); //получаем мячик
-const FIELD_Y_START_POINT = 0; // начало поля по Y
-const FIELD_Y_END_POINT = 281; // конец поля по Y
-const GOAL_Y_START_POINT = 115; //начало ворот по Y оси
-const GOAL_Y_END_POINT = 145; //конец ворот по Y оси
-const BALL_X_START_POS = 17; //начальное положение мячи по X
-const BALL_X_END_POS = 445; //дистацния до противоположной стороны по X
+const FIELD_Y_START_COORD = 0; // начало поля по Y
+const FIELD_Y_END_COORD = 281; // конец поля по Y
+const FIELD_X_START_COORD = 17; //начало поля по X
+const FIELD_X_END_COORD = 445; //конец поля по X
+const GOAL_Y_START_COORD = 115; //начало ворот по Y оси
+const GOAL_Y_END_COORD = 145; //конец ворот по Y оси
 const ANIMATION_DURATION = 1000; //скорость анимации
-let currentBallPos = BALL_X_START_POS; //настоящее положение мяча
+let currentBallPos = FIELD_X_START_COORD; //настоящее положение мяча
 
 function getRandomNumber(startRange, endRange) {
   return Math.floor(Math.random() * (endRange - startRange) + startRange);
@@ -22,16 +22,16 @@ function coreGame(currentBallPos, distance) {
     {
       left: (currentBallPos += distance),
       top: (randomYposition = getRandomNumber(
-        FIELD_Y_START_POINT,
-        FIELD_Y_END_POINT
+        FIELD_Y_START_COORD,
+        FIELD_Y_END_COORD
       ))
     },
     {
       duration: ANIMATION_DURATION,
       complete: function() {
         if (
-          randomYposition >= GOAL_Y_START_POINT &&
-          randomYposition <= GOAL_Y_END_POINT
+          randomYposition >= GOAL_Y_START_COORD &&
+          randomYposition <= GOAL_Y_END_COORD
         ) {
           console.log('goal!');
         }
@@ -43,9 +43,9 @@ function coreGame(currentBallPos, distance) {
 
 ballElem.on('click', function() {
   //по клику проверяем положение меча и передаем значение с + и -
-  if (currentBallPos <= BALL_X_START_POS) {
-    currentBallPos = coreGame(currentBallPos, BALL_X_END_POS);
+  if (currentBallPos <= FIELD_X_START_COORD) {
+    currentBallPos = coreGame(currentBallPos, FIELD_X_END_COORD);
   } else {
-    currentBallPos = coreGame(currentBallPos, -BALL_X_END_POS);
+    currentBallPos = coreGame(currentBallPos, -FIELD_X_END_COORD);
   }
 });
